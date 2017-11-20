994218cb8cfc5aec12393891b20347bc0b21675c
Ejnar Arechavala <ejnar123@gmail.com>
2017-11-20 12:01:22 -0500

    Code Commented

--- client.py
+++ client.py
@@ -15,45 +15,49 @@ def receive(socket):
 		return False
 	msg_flag = ord(r) #convert the msg_flag
 	while(msg_flag > 0): #read all messages from the client
-		server_msg = socket.recv(msg_flag)
-		print server_msg
-		msg_flag = socket.recv(1)
-		if(len(msg_flag) == 0):
+		server_msg = socket.recv(msg_flag) #read from buffer
+		print server_msg #print whats in buffer
+		msg_flag = socket.recv(1) #read next flag
+		if(len(msg_flag) == 0): #if connection has been closed
 			return False
-		msg_flag = ord(msg_flag)
+		msg_flag = ord(msg_flag) #convert next flag
 	#continue when word_flag is 0 i.e. a control packet
-	word_len = ord(socket.recv(1))
-	num_wrong = ord(socket.recv(1))
-	if word_len > 0:
+	word_len = ord(socket.recv(1)) #read word length as specified in format
+	num_wrong = ord(socket.recv(1)) #read number of wrong guesses 
+	if word_len > 0: #double check there is a word to read
 		board = list(socket.recv(word_len))
-	if num_wrong > 0:
+	if num_wrong > 0: #double check there are wrong letters to read
 		wrong_letters = list(socket.recv(num_wrong))
 	return True
 
 def guess_valid(guess):
-	guess = guess.lower()
-	if ((not (re.match('^[a-z]*$', guess))) or len(guess) > 1): #if not a letter
+	guess = guess.lower() #lowercase the guess
+	if ((not (re.match('^[a-z]*$', guess))) or len(guess) > 1):
+	#if the guess is not a latter between a and z or the guess is more than one letter
 		print 'Error! Please guess one letter.'
 		return False	
 	if((guess in wrong_letters) or (guess in board)):
+	#if the guess has been guessed correctly or incorrectly
 		return 'Error! Letter '+ str(guess) + ' has been guessed before, please guess another letter.'
 		return False
-	return True
+	return True #it is a valid guess
 
 
 def main():
 	global board, wrong_letters, num_wrong, word_len
 	argc = len(sys.argv)
-	if (argc != 3):
+	if (argc != 3): #make sure right number of args
 		print 'USAGE: python client.py <IP address> <port number>'
 		sys.exit()
 	try:
+		#attempt to connect to the socket
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create new socket
 		s.connect((sys.argv[1], int(sys.argv[2]))) #connect to ip and port
 	except socket.error:
 		print 'There was an error with connecting!'
 		sys.exit()
 
+	#just for first message which asks for user input to start game
 	msg_bytes = ord(s.recv(1)) #read length of first message
 	rec_msg = s.recv(msg_bytes) #recieve the message
 	snd_msg = raw_input(rec_msg) #print message, ask user for input
@@ -61,22 +65,23 @@ def main():
 		snd_msg = raw_input(rec_msg) #print message, ask user for input
 	if snd_msg == 'y':
 		send_msg_pkt(s, '') #send packet with msg_flag(0)
-		print ''
+		print '' #print blank line
 	else:
 		s.close()
 		sys.exit()
 
 	while(receive(s)):
-		print ' '.join(board)
-		print 'Incorrect Guesses: ' + ' '.join(wrong_letters).upper() + '\n'
-		guess = raw_input("Letter to Guess: ")
-		while(not guess_valid(guess)):
+		print ' '.join(board) #prints board with spaces between the letters/underscores
+		print 'Incorrect Guesses: ' + ' '.join(wrong_letters) + '\n' #same as above for wrong guesses
+		guess = raw_input("Letter to Guess: ") #ask client to guess a letter
+		while(not guess_valid(guess)): #keep asking until guess is valid to send
 			guess = raw_input("Letter to Guess: ")
-		send_msg_pkt(s, guess)
+		send_msg_pkt(s, guess) #send the guess
 
 	s.close()
 
+
+
 # Starts main
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
--- server.py
+++ server.py
@@ -27,6 +27,7 @@ class Game(object):
 			if len(self.wrong_letters) == 6:
 				self.game_over = True
 
+
 def send_ctrl_pkt(socket, game):
 	msg_flag = chr(0)
 	word_len = chr(len(game.word))
@@ -41,23 +42,29 @@ def send_msg_pkt(socket, data):
 	# print n, "bytes sent"
 
 def receive_msg_pkt(socket):
-	r = socket.recv(1)
-	if(len(r) == 0):
+	r = socket.recv(1) #read first byte (msg_flag)
+	if(len(r) == 0): #if nothing, client has disconnected
 		return ''
 	else:
+		#get msg_flag and read that amount from socket
 		msg_flag = ord(r)
-		return socket.recv(msg_flag)
+		if(msg_flag == 0):
+			return msg_flag #theoretically should only be the start_msg from client
+		else:
+			return socket.recv(msg_flag)
 
 
 def run_game(socket, g):
-	connected = True
-	while(not g.game_over):
-		send_ctrl_pkt(socket, g)
-		msg = receive_msg_pkt(socket)
+	connected = True #for seeing if client disconnected
+	while((not g.game_over) and connected): #while game running
+		send_ctrl_pkt(socket, g) #send it current state of the game
+		msg = receive_msg_pkt(socket) #wait for response i.e the guess
 		if(len(msg) == 0): #if disconnected
 			connected = False
-		g.guess(msg)
-	if connected:
+		else:
+			g.guess(msg)
+	if connected: #if still connected when game ends/disconnects
+		#send game packets
 		if (g.game_won):
 			send_msg_pkt(socket, 'You Win!')
 		else:
@@ -68,19 +75,20 @@ def run_game(socket, g):
 
 def on_new_client(socket, addr):
 	global running_games
+	#when new client connected, ask to start game
 	send_msg_pkt(socket,'Ready to start game? (y/n): ')
-	rec_msg = socket.recv(1)
-	if len(rec_msg) != 0:
-		rec_msg = ord(rec_msg)
-		if(rec_msg == 0): #if recieve packet with msg_flag = 0
-			if(running_games >= 3):
-				send_msg_pkt(socket, 'Server-Overloaded') #then close connection
-			else:
-				g = Game(random.choice(words))
-				print 'New Game Started!'
-				running_games += 1
-				run_game(socket, g)
-				running_games -= 1 #whenever game is finnished running
+	# rec_msg = socket.recv(1)
+	# if len(rec_msg) != 0:
+	start_msg = receive_msg_pkt(socket)
+	if start_msg == 0: #if it is a packet with msg_flag of 0
+		if(running_games >= 3): #if 3 or more games already running
+			send_msg_pkt(socket, 'Server-Overloaded') #then close connection
+		else:
+			#start a new game
+			g = Game(random.choice(words))
+			running_games += 1
+			run_game(socket, g) #run game g
+			running_games -= 1 #whenever game is finnished running
 	socket.close()
 	print 'Connection Ended with', addr
 
@@ -91,12 +99,12 @@ def main():
 	if (argc != 2 and argc != 3):
 		print 'USAGE: python server.py <port number> [dictionary file name]'
 		sys.exit()
-	if(argc == 3):
+	if(argc == 3): #if 3 args, read the third which will be text file name
 		with open(sys.argv[2]) as f:
 			words = f.read().splitlines()[1:] #read lines into list
 	else: #argc == 2, use default word dictionary
 		words = ['jazz', 'buzz', 'hajj', 'fizz', 'jinx', 'huffs', 'buffs', 'jiffs', 'junks', 'hello', 'puzzle', 'buzzed', 'huzzah', 'fizzed', 'jumped']
-	host = 'localhost' # start on 127.0.0.1
+	host = 'localhost' # start on 127.0.0.1, 'localhost' is an optimization
 	port = int(sys.argv[1]) #port number from input
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create new socket
 	s.bind((host, port)) #bind host and port
@@ -105,7 +113,7 @@ def main():
 	print 'Listening for Connections...'
 	while True:
 		client, addr = s.accept()     # Establish connection with client.
-		print 'Got connection from', addr
+		print 'Connection Started with', addr
 		thread.start_new_thread(on_new_client,(client,addr)) #start new thread with client socket
 	s.close()
 
